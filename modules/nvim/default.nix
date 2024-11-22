233edb8bda3565d9a3f5c6b938f579a0e0113609
orbital4 <github.b5jge@passmail.net>
2024-11-22 12:19:47 -0300

    feat(nvim): Replace autopairs with mini.pairs and add other symbols

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -234,17 +234,26 @@
         
         plugins.web-devicons.enable = true;
 
-        plugins.nvim-autopairs = {
+        plugins.mini = {
             enable = true;
-            settings = {
-                fast_wrap.chars = [
-                    "{"
-                    "["
-                    "("
-                    "\""
-                    "'"
-                    "$"
-                ];
+            modules = {
+                pairs = {
+                    modes = { insert = true; command = false; terminal = false; };
+                    mappings = {
+                        "(" = { action = "open"; pair = "()"; neigh_pattern = "[^\\]."; };
+                        "[" = { action = "open"; pair = "[]"; neigh_pattern = "[^\\]."; };
+                        "{" = { action = "open"; pair = "{}"; neigh_pattern = "[^\\]."; };
+                        "<" = { action = "open"; pair = "<>"; neigh_pattern = "[^\\]."; };
+                        ")" = { action = "close"; pair = "()"; neigh_pattern = "[^\\]."; };
+                        "]" = { action = "close"; pair = "[]"; neigh_pattern = "[^\\]."; };
+                        "}" = { action = "close"; pair = "{}"; neigh_pattern = "[^\\]."; };
+                        ">" = { action = "close"; pair = "<>"; neigh_pattern = "[^\\]."; };
+                        "\$" = { action = "closeopen"; pair = "\$\$"; neigh_pattern = "[^\\]."; register = { cr = false; }; };
+                        "\"" = { action = "closeopen"; pair = "\"\""; neigh_pattern = "[^\\]."; register = { cr = false; }; };
+                        "`" = { action = "closeopen"; pair = "``"; neigh_pattern = "[^\\]."; register = { cr = false; }; };
+                        "'" = { action = "closeopen"; pair = "''"; neigh_pattern = "[^%a\\]."; register = { cr = false; }; };
+                    };
+                };
             };
         };
 
